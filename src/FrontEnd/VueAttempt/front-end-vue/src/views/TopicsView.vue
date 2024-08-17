8e65ce04abe05187ddd25606a2295f8e59456ee7
Michael Render <render.michael@gmail.com>
2024-08-16 21:45:58 -0400

    Create Vue front-end

---
+++ src/FrontEnd/VueAttempt/front-end-vue/src/views/TopicsView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import TopicItem from '../components/TopicItem.vue'
+</script>
+
+<template>
+  <div class="topics">
+    <h1>Here are all the topics</h1>
+  </div>
+  <TopicItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Community</template>
+
+    Got stuck? Ask your question on
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
+    Discord server, or
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
+      >StackOverflow</a
+    >. You should also subscribe to
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
+    the official
+    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
+    twitter account for latest news in the Vue world.
+  </TopicItem>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
